e3a6466b24fe1d201110acb73b79346ca765d853
Lucas Song <yuanzheng@cs.byu.edu>
2019-01-08 21:53:15 +0800

    updated code for supporting the rod length that is not in the price list

--- DynamicProgrammingDemo/RodCutting.java
+++ DynamicProgrammingDemo/RodCutting.java
@@ -8,23 +8,26 @@ public class RodCutting {
             return 0;
         }
 
-        // Cannot find the prices for the piece of whole rod
-        if (length <=0 || length >= prices.length) {
+        if (length <=0) {
             return 0;
         }
 
         // Keep the best price for each part in different sizes (from 0 to 'length').
         int[] tmp = new int[length + 1];
-
         tmp[0] = 0;
 
         // Compute the best price for each size of rod
         for (int i=1; i<=length; i++) {
             int best = Integer.MIN_VALUE;
 
-            for (int j=1; j<=i; j++) {
-                int price = prices[j] + tmp[i-j];
+            for (int j=1; j<i; j++) {
+                int price = tmp[j] + tmp[i-j];
                 best = Math.max(best, price);
+            }
+
+            // find the price of length, if we can.
+            if (i <= prices.length) {
+                best = Math.max(best, prices[i-1]);
             }
 
             tmp[i] = best;
@@ -38,29 +41,18 @@ public class RodCutting {
     public static void main(String[] args) {
         RodCutting test1 = new RodCutting();
 
-        int[] prices = {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
-        int length = 9;
-        System.out.println("Rod cuttingm test1: " + length + " => " +
-                test1.cutForBestPrice(prices, length));
-
-        System.out.println("Rod cutting test2: " + length + " => " +
+        int[] prices = {1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+        
+        int length = 4;
+        System.out.println("Rod cutting test: " + length + " => " +
                 test1.cutForBestPrice(null, length));
 
-        length = 0;
-        System.out.println("Rod cutting test3: " + length + " => " +
+        for (int i=0; i<= (prices.length + 15); i++) {
+            length = i;
+            System.out.println("Rod cutting test" + i + ": " + length + " => " +
                 test1.cutForBestPrice(prices, length));
 
-        length = 1;
-        System.out.println("Rod cutting test4: " + length + " => " +
-                test1.cutForBestPrice(prices, length));
-
-        length = 10;
-        System.out.println("Rod cutting test5: " + length + " => " +
-                test1.cutForBestPrice(prices, length));
-
-        length = 11;
-        System.out.println("Rod cutting test6: " + length + " => " +
-                test1.cutForBestPrice(prices, length));
+        }
 
     }
 }
